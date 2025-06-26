9aac6c622761125a086b12ee52f6ac2f14e84baa
striderarun <strider90arun@gmail.com>
2025-06-26 14:23:07 -0700

    AstPrinter and Parser impl

---
+++ src/main/java/com/arun/interpreter/lox/ast/AstPrinter.java
@@ -0,0 +1,75 @@
+package com.arun.interpreter.lox.ast;
+
+import com.arun.interpreter.lox.Token;
+import com.arun.interpreter.lox.TokenType;
+
+public class AstPrinter implements Visitor<String> {
+
+    public String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parenthesize("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+    private String parenthesize(String name, Expr... exprs) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(").append(name);
+        for (Expr expr : exprs) {
+            builder.append(" ");
+            builder.append(expr.accept(this));
+        }
+        builder.append(")");
+
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        AstPrinter astPrinter = new AstPrinter();
+        // (* (- 123) (group 45.67))
+        Expr expression1 = new Expr.Binary(
+                new Expr.Unary(
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(123)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                        new Expr.Literal(45.67)));
+
+        System.out.println(astPrinter.print(expression1));
+
+        // (1+2) * (4-3)
+        Expr expression2 = new Expr.Binary(
+                new Expr.Binary(
+                        new Expr.Literal(1),
+                        new Token(TokenType.PLUS, "+", null, 1),
+                        new Expr.Literal(2)
+                ),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Binary(
+                        new Expr.Literal(4),
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(3)
+                )
+        );
+        System.out.println(astPrinter.print(expression2));
+    }
+}
